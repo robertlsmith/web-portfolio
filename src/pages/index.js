41512ac05285aafa3650c46416286fbcda4f3705
Robert Smith <thersmith09@gmail.com>
2023-08-14 21:07:03 -0700

    Started laying out nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,27 @@
 import * as React from "react"
 
+const cursor = document.querySelector(".cursor");
+
 const IndexPage = () => {
   return (
-    <main>
-      <h1>Hallo Welt!</h1>
+    <main class="bg-persian-indigo">
+      <div class="container grid grid-cols-[1fr_1fr_1fr] grid-rows-[1fr_1fr_1fr] gap-x-20 p-20">
+        <div class="logo-div">
+          <a class="text-9xl font-medium text-antiflash-white" href="/">Robert Smith</a>
+        </div>
+        <div class="nav-div text-antiflash-white">
+          <nav>
+            <ul>
+              <li>
+                <a class="hover:line-through" href="/work">Work</a>
+              </li>
+              <li>
+                <a class="hover:line-through" href="/contact">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     `./src/components/**/*.{js,jsx,ts,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: ["'Raleway', sans-serif"],
+      },
+      colors: {
+        'persian-indigo': '#27187e',
+        'antiflash-white': '#f1f2f6',
+        'cornflower-blue': '#758BFD',
+        'periwinkle': '#AEB8FE',
+        'ut-orange': '#FF8600',
+      },
+    },
   },
   plugins: [],
 }
