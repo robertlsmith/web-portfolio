52674ea26ead86b8e81d463faa3ac261cf4161fa
Robert Smith <thersmith09@gmail.com>
2023-08-14 18:29:19 -0700

    Updated README

---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const ContactPage = () => {
+    return (
+      <main>
+        <h1>Hallo Kontakt</h1>
+      </main>
+    )
+  }
+  
+  export default ContactPage
+  
+  export const Head = () => <title>Robert Smith</title>
---
+++ src/pages/work.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const WorkPage = () => {
+    return (
+      <main>
+        <h1>Hallo Arbeit</h1>
+      </main>
+    )
+}
+  
+  export default WorkPage
+  
+  export const Head = () => <title>Robert Smith</title>
