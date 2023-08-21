6dfe42a9741f7b82c8ebad8c85c9d0564c2a3428
Robert Smith <thersmith09@gmail.com>
2023-08-20 19:38:10 -0700

    Added Navbar, but will redo using React-DOM Routing

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,9 +2,20 @@ import * as React from "react"
 
 function NavBar() {
     return (
-        <div class="nav">
-            <p>Nav bar text</p>
+        <div class="nav-wrapper">
+            <div class="nav">
+                <ul>
+                    <li><a class="logo" href="/">RS.</a></li>
+                    <div class="nav-right">
+                        {/* Space here for adding the light/dark mode button */}
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="contact">Contact</a></li>
+                    </div>         
+                </ul>
+            </div>
         </div>
+        
     )
 }
 
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,10 @@
 import * as React from "react"
+import NavBar from "../components/navbar"
 
 const ContactPage = () => {
     return (
       <main>
+        <NavBar></NavBar>
         <h1>Hallo Kontakt</h1>
       </main>
     )
